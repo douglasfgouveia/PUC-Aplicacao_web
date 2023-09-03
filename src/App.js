47bafd29bfd368def3f6cf9492d3f89d9bdb80f7
Douglas F. Gouveia <df_gouveia@hotmail.com>
2023-09-03 17:38:29 -0300

    Update: added label to App{Acesso com sucesso}

--- src/App.js
+++ src/App.js
@@ -1,3 +1,69 @@
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   const users = {
+//     'douglas@testpuc.com.br': 'password1',
+//     'user2': 'password2',
+//   };
+
+//   const handleLogin = () => {
+//     if (users.hasOwnProperty(username) && users[username] === password) {
+//       setLoggedIn(true);
+//     } else if (username === '' || password === '') {
+//       alert('Preencha ambos os campos.');
+//     } else {
+//       alert('Credenciais inválidas. Tente novamente.');
+//     }
+//   };
+  
+
+//   const handleLogout = () => {
+//     setLoggedIn(false);
+//     setUsername('');
+//     setPassword('');
+//   };
+
+//   if (loggedIn) {
+//     return (
+//       <div className="App">
+//         <h1>Bem-vindo, {username}!</h1>
+//         <button onClick={handleLogout}>Logout</button>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div className="App">
+//         <h1>Login</h1>
+//         <div>
+//         <input
+//             type="text"
+//             placeholder="Usuário"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//         <input
+//           type="password"
+//           placeholder="Senha"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </div>
+               
+//         <button onClick={handleLogin}>Acessar</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import './App.css';
 
@@ -20,7 +86,6 @@ function App() {
       alert('Credenciais inválidas. Tente novamente.');
     }
   };
-  
 
   const handleLogout = () => {
     setLoggedIn(false);
@@ -28,38 +93,38 @@ function App() {
     setPassword('');
   };
 
-  if (loggedIn) {
-    return (
-      <div className="App">
-        <h1>Bem-vindo, {username}!</h1>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <h1>Login</h1>
+  return (
+    <div className="App">
+      {loggedIn ? (
         <div>
-        <input
-            type="text"
-            placeholder="Usuário"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <h1>Bem-vindo, {username}!</h1>
+          <p>Acesso com sucesso</p> {/* Label de acesso com sucesso */}
+          <button onClick={handleLogout}>Logout</button>
         </div>
+      ) : (
         <div>
-        <input
-          type="password"
-          placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          />
+          <h1>Login</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button onClick={handleLogin}>Acessar</button>
         </div>
-               
-        <button onClick={handleLogin}>Acessar</button>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 export default App;
